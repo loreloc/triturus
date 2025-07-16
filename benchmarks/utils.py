b73e777a8b1470931c7c98992d687550d9c8b614
loreloc <lorenzoloconte@outlook.it>
2025-07-16 11:54:28 +0100

    changed eval_gbps function as to consider the number of bytes per element (defaults=4 as for fp32)

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -10,5 +10,6 @@ def eval_tflops(nflops: int, ms: float) -> float:
     return (nflops * 1e-12) / (ms * 1e-3)
 
 
-def eval_gbps(size: int, ms: float) -> float:
-    return (size * 1e-9) / (ms * 1e-3)
+def eval_gbps(size: int, ms: float, *, bpe: int = 4) -> float:
+    return ((size * bpe) * 1e-9) / (ms * 1e-3)
+
