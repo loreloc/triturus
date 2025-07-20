a2effa8b7b46ba1b528d1e136441aece548736ee
loreloc <lorenzoloconte@outlook.it>
2025-07-20 14:25:01 +0100

    updated configs for logmm2exp kernel

--- triturus/logmm2exp.py
+++ triturus/logmm2exp.py
@@ -4,34 +4,64 @@ import triton.language as tl
 
 CONFIGS = [
     triton.Config(
-        {"BLOCK_SIZE": 32, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 8},
+        {"BLOCK_SIZE_M": 32, "BLOCK_SIZE_N": 16, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
         num_stages=6,
         num_warps=1,
     ),
     triton.Config(
-        {"BLOCK_SIZE": 32, "BLOCK_SIZE_K": 32, "GROUP_SIZE": 8},
+        {"BLOCK_SIZE_M": 32, "BLOCK_SIZE_N": 32, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
+        num_stages=6,
+        num_warps=1,
+    ),
+    triton.Config(
+        {"BLOCK_SIZE_M": 32, "BLOCK_SIZE_N": 32, "BLOCK_SIZE_K": 32, "GROUP_SIZE": 4},
         num_stages=2,
         num_warps=4,
     ),
     triton.Config(
-        {"BLOCK_SIZE": 32, "BLOCK_SIZE_K": 32, "GROUP_SIZE": 8},
+        {"BLOCK_SIZE_M": 32, "BLOCK_SIZE_N": 32, "BLOCK_SIZE_K": 32, "GROUP_SIZE": 4},
         num_stages=4,
         num_warps=4,
     ),
     triton.Config(
-        {"BLOCK_SIZE": 64, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 8},
+        {"BLOCK_SIZE_M": 64, "BLOCK_SIZE_N": 32, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
         num_stages=2,
         num_warps=4,
     ),
     triton.Config(
-        {"BLOCK_SIZE": 64, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 8},
+        {"BLOCK_SIZE_M": 64, "BLOCK_SIZE_N": 32, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
         num_stages=4,
         num_warps=4,
     ),
     triton.Config(
-        {"BLOCK_SIZE": 64, "BLOCK_SIZE_K": 32, "GROUP_SIZE": 8},
+        {"BLOCK_SIZE_M": 64, "BLOCK_SIZE_N": 64, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
         num_stages=2,
         num_warps=4,
+    ),
+    triton.Config(
+        {"BLOCK_SIZE_M": 64, "BLOCK_SIZE_N": 64, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
+        num_stages=4,
+        num_warps=4,
+    ),
+    triton.Config(
+        {"BLOCK_SIZE_M": 64, "BLOCK_SIZE_N": 64, "BLOCK_SIZE_K": 32, "GROUP_SIZE": 4},
+        num_stages=4,
+        num_warps=4,
+    ),
+    triton.Config(
+        {"BLOCK_SIZE_M": 128, "BLOCK_SIZE_N": 16, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
+        num_stages=3,
+        num_warps=2,
+    ),
+    triton.Config(
+        {"BLOCK_SIZE_M": 128, "BLOCK_SIZE_N": 32, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
+        num_stages=3,
+        num_warps=4,
+    ),
+    triton.Config(
+        {"BLOCK_SIZE_M": 256, "BLOCK_SIZE_N": 32, "BLOCK_SIZE_K": 16, "GROUP_SIZE": 4},
+        num_stages=3,
+        num_warps=8,
     ),
 ]
 
@@ -54,7 +84,8 @@ def _ker_logmm2exp(
     m: int,
     k: int,
     n: int,
-    BLOCK_SIZE: tl.constexpr,  # The block size
+    BLOCK_SIZE_M: tl.constexpr,  # The block size along axis=0 of A
+    BLOCK_SIZE_N: tl.constexpr,  # The block size along axis=1 of B
     BLOCK_SIZE_K: tl.constexpr,  # The block size along the dimension to contract
     GROUP_SIZE: tl.constexpr,  # The group size used for swizzling
     TILE_SIZE: tl.constexpr,  # The size of a tile column of B, i.e., next_power_of_2(k)
@@ -62,28 +93,39 @@ def _ker_logmm2exp(
 ):
     # Retrieve the program ids on a 2D grid
     pid = tl.program_id(axis=0)
-    num_programs0 = tl.cdiv(m, BLOCK_SIZE)
-    num_programs1 = tl.cdiv(n, BLOCK_SIZE)
+    num_programs0 = tl.cdiv(m, BLOCK_SIZE_M)
+    num_programs1 = tl.cdiv(n, BLOCK_SIZE_N)
     pid_i = pid // num_programs1
     pid_j = pid % num_programs1
     pid_i, pid_j = tl.swizzle2d(pid_i, pid_j, num_programs0, num_programs1, GROUP_SIZE)
+    # Hint some information to the compiler
+    tl.assume(pid_i >= 0)
+    tl.assume(pid_j >= 0)
+    tl.assume(m > 0)
+    tl.assume(n > 0)
+    tl.assume(k > 0)
+    tl.assume(a_str0 > 0)
+    tl.assume(a_str1 > 0)
+    tl.assume(b_str0 > 0)
+    tl.assume(b_str1 > 0)
+    tl.assume(c_str0 > 0)
+    tl.assume(c_str1 > 0)
     # Compute the number of blocks to multiply and accumulate, and the block indices
-    block_idx = tl.arange(0, BLOCK_SIZE)
-    block_idx_k = tl.arange(0, BLOCK_SIZE_K)
     num_blocks = tl.cdiv(k, BLOCK_SIZE_K)
+    block_idx_k = tl.arange(0, BLOCK_SIZE_K)
     # Compute the pointers to the starting blocks of A (along axis=0) and B (along axis=1)
-    a_offs0 = (pid_i * BLOCK_SIZE + block_idx) % m
-    b_offs1 = (pid_j * BLOCK_SIZE + block_idx) % n
+    a_offs0 = (pid_i * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % m
+    b_offs1 = (pid_j * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % n
     # Compute the maximum values
     tile_idx = tl.arange(0, TILE_SIZE) % n
-    m_ptrs = b_ptr + tile_idx[:, None] * b_str0 + b_offs1[None, :] * b_str1
-    b_tile = tl.load(m_ptrs)
+    b_tile_ptrs = b_ptr + tile_idx[:, None] * b_str0 + b_offs1[None, :] * b_str1
+    b_tile = tl.load(b_tile_ptrs)
     max_block = tl.clamp(tl.max(b_tile, axis=0), -1e38, 1e38)
     # Multiply by the strides for A and B along axes 0 and 1 as to retrieve the pointers
     a_ptrs = a_ptr + a_offs0[:, None] * a_str0 + block_idx_k[None, :] * a_str1
     b_ptrs = b_ptr + block_idx_k[:, None] * b_str0 + b_offs1[None, :] * b_str1
     # Instantiate the accumulator
-    acc = tl.zeros((BLOCK_SIZE, BLOCK_SIZE), dtype=tl.float32)
+    acc = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
     # Compute and accumulate the dot products of block matrices
     for _ in range(num_blocks - 1):
         # Load the blocks
@@ -91,11 +133,10 @@ def _ker_logmm2exp(
         b_block = tl.load(b_ptrs)
         # Exponentiate the values in the B matrix ...
         # ... but first subtract the maximum values for numerical stability
-        exp_b_block = tl.exp(b_block - max_block)
         # Compute the dot product of blocks
         acc = tl.dot(
             a_block,
-            exp_b_block,
+            tl.exp(b_block - max_block),
             acc=acc,
             input_precision=PRECISION,
         )
@@ -109,11 +150,10 @@ def _ker_logmm2exp(
     a_block = tl.load(a_ptrs, mask=mask[None, :], other=0.0)
     b_block = tl.load(b_ptrs, mask=mask[:, None], other=0.0)
     # Exponentiate the values in the B matrix
-    exp_b_block = tl.exp(b_block - max_block)
     # Compute the dot product of blocks
     acc = tl.dot(
         a_block,
-        exp_b_block,
+        tl.exp(b_block - max_block),
         acc=acc,
         input_precision=PRECISION,
     )
@@ -142,8 +182,8 @@ def logmm2exp(
     tile_size = triton.next_power_of_2(b.shape[0])
     # Launch the kernel
     grid = lambda meta: (
-        triton.cdiv(a.shape[0], meta["BLOCK_SIZE"])
-        * triton.cdiv(b.shape[1], meta["BLOCK_SIZE"]),
+        triton.cdiv(a.shape[0], meta["BLOCK_SIZE_M"])
+        * triton.cdiv(b.shape[1], meta["BLOCK_SIZE_N"]),
     )
     _ker_logmm2exp[grid](
         a,
